6d83f7f395bd18ed56c17398f4c0ac291d01d84e
Sasha Mihalache <sasha.mihalache@gmail.com>
2023-08-10 10:28:31 +0300

    add dashboard

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,6 +12,7 @@ import {
 } from 'lucide-react';
 import { Montserrat } from 'next/font/google';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 const montserrat = Montserrat({
@@ -35,25 +36,25 @@ const routes = [
   {
     label: 'Image Generation',
     icon: ImageIcon,
-    href: '/image-generation',
+    href: '/image',
     color: 'text-pink-700',
   },
   {
     label: 'Video Generation',
     icon: VideoIcon,
-    href: '/video-generation',
+    href: '/video',
     color: 'text-orange-500',
   },
   {
     label: 'Music Generation',
     icon: Music,
-    href: '/music-generation',
+    href: '/music',
     color: 'text-emerald-500',
   },
   {
     label: 'Code Generation',
     icon: Code,
-    href: '/code-generation',
+    href: '/code',
     color: 'text-green-500',
   },
   {
@@ -64,6 +65,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -81,7 +84,12 @@ const Sidebar = () => {
               <Link
                 href={r.href}
                 key={r.href}
-                className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                className={cn(
+                  'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                  pathname === r.href
+                    ? 'text-white bg-white/10'
+                    : 'text-zinc-400'
+                )}
               >
                 <div className="flex items-center flex-1">
                   <r.icon className={cn('h-5 w-5 mr-3', r.color)} />
