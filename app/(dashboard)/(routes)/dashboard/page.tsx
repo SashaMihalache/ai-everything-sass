9219cab8ebd00864c46d3421c186be009c1c27cb
Sasha Mihalache <sasha.mihalache@gmail.com>
2023-08-10 10:15:42 +0300

    add layout and sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,10 +2,5 @@ import { Button } from '@/components/ui/button';
 import { UserButton } from '@clerk/nextjs';
 
 export default function DashboardPage() {
-  return (
-    <div>
-      <p>Dashboard Page (Private)</p>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
