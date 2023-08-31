349e417d4b5dc585ea76cca67dc713b221436743
Sasha Mihalache <sasha.mihalache@gmail.com>
2023-08-31 18:03:50 +0300

    fix landing page content

--- components/common/LandingContent.tsx
+++ components/common/LandingContent.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { cn } from '@/lib/utils';
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 
 const testimonials = [
@@ -10,43 +11,43 @@ const testimonials = [
     description: "This is the best application I've used!",
   },
   {
-    username: 'John',
+    name: 'John',
     avatar: 'J',
     title: 'Software Developer',
     description: 'This application is amazing!',
   },
   {
-    username: 'Jane',
+    name: 'Jane',
     avatar: 'J',
     title: 'Frontend Developer',
     description: 'I love the user interface of this application!',
   },
   {
-    username: 'Bob',
+    name: 'Bob',
     avatar: 'B',
     title: 'Full Stack Developer',
     description: 'This application has made my life so much easier!',
   },
   {
-    username: 'Alice',
+    name: 'Alice',
     avatar: 'A',
     title: 'Software Engineer',
     description: 'I highly recommend this application to anyone!',
   },
   {
-    username: 'David',
+    name: 'David',
     avatar: 'D',
     title: 'Backend Developer',
     description: 'This application has saved me so much time!',
   },
   {
-    username: 'Emily',
+    name: 'Emily',
     avatar: 'E',
     title: 'Web Developer',
     description: 'This application is a game changer!',
   },
   {
-    username: 'Mike',
+    name: 'Mike',
     avatar: 'M',
     title: 'Software Architect',
     description: 'This application is the best I have ever used!',
@@ -66,10 +67,18 @@ export const LandingContent = () => {
             className="bg-[#192339] border-none text-white"
           >
             <CardHeader>
-              <CardTitle className="flex items-center gap-x-2">
+              <CardTitle className="flex items-center w-full justify-between gap-x-2">
                 <div>
                   <p className="text-lg">{testimonial.name}</p>
                   <p className="text-zinc-400 text-sm">{testimonial.title}</p>
+                </div>
+                <div
+                  className={cn(
+                    'flex items-center justify-center w-10 h-10 rounded-full',
+                    'bg-gradient-to-r from-purple-500 to-pink-600'
+                  )}
+                >
+                  <p className="text-lg">{testimonial.avatar}</p>
                 </div>
               </CardTitle>
               <CardContent className="pt-4 px-0">
